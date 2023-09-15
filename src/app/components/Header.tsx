e174504dfb67091b35fdca849924621eed6205db
Wassim Triki <wsm@gmail.com>
2023-09-15 19:58:44 +0100

    setup basic layout

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-white border p-4 rounded-xl overflow-hidden col-span-2">
+      Header
+    </div>
+  );
+};
+
+export default Header;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`bg-[#FAFAFA] min-h-screen p-5 ${inter.className}`}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
