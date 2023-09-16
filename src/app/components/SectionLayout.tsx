99d0bda4da574fa963b20813cfa424d5660ca45a
Wassim Triki <wsm@gmail.com>
2023-09-16 05:50:53 +0100

    fix spacing

--- src/app/components/SectionLayout.tsx
+++ src/app/components/SectionLayout.tsx
@@ -11,7 +11,7 @@ const SectionLayout = ({
   children,
 }: SectionLayoutProps) => {
   return (
-    <div className="p-4 flex flex-col gap-8 ">
+    <div className="p-8 flex flex-col gap-8 ">
       <div className="flex flex-col gap-4">
         <h2 className="font-bold text-3xl">{title}</h2>
         <p className="text-sm text-muted-foreground">{description}</p>
