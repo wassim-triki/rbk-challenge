7608100b01603325e767491cf6b365ec343699eb
Wassim Triki <wsm@gmail.com>
2023-09-16 03:09:46 +0100

    modify layout max width

--- src/app/components/LinkPagesLayout.tsx
+++ src/app/components/LinkPagesLayout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Container from "./Container";
+import Image from "next/image";
 
 const LinkPagesLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -7,7 +8,16 @@ const LinkPagesLayout = ({ children }: { children: React.ReactNode }) => {
       <Container>
         <div className="flex gap-5 mt-5">
           <div className="w-full h-ful border p-4 flex-1  rounded-xl bg-white ">
-            Phone
+            <div className="bg-red-50s0 m-16">
+              <Image
+                className="w-full object-cover opacity-60"
+                src={"/iphone.png"}
+                alt="iphone"
+                width={100}
+                height={100}
+                quality={95}
+              />
+            </div>
           </div>
           <div className="w-full h-ful border p-4 flex-[2]  rounded-xl bg-white ">
             {children}
